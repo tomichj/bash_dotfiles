093a63a26f7f661db02dad7a783765759da6e65c
Justin Tomich <tomichj@gmail.com>
2014-08-21 15:01:03 -0700

    fixed rbenv-completions so it sources instead of executes

--- ruby/rbenv-completions.bash
+++ ruby/rbenv-completions.bash
@@ -1,6 +1,6 @@
 # try homebrew first
 if hash brew 2>/dev/null && [ -f `brew --prefix`/opt/rbenv/completions/rbenv.bash ]; then
   source `brew --prefix`/opt/rbenv/completions/rbenv.bash
-elif [[ -f ~/.rbenv/completions/rbenv.bash ]]; then
-    ~/.rbenv/completions/rbenv.bash
+elif [[ -e ~/.rbenv/completions/rbenv.bash ]]; then
+    . ~/.rbenv/completions/rbenv.bash
 fi
