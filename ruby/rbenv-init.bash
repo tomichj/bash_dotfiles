22a514440e4b87e6f1499062cbad0c9a413d5a0e
Justin Tomich <tomichj@gmail.com>
2015-05-21 12:25:45 -0700

    rbenv-init now appends ~/.rbenv/bin to path if you do not have rbenv in your path and that dir exists

--- ruby/rbenv-init.bash
+++ ruby/rbenv-init.bash
@@ -1,5 +1,8 @@
 # installed via homebrew, don't need path
-# export PATH="$HOME/.rbenv/bin:$PATH"
+
+if ! hash rbenv 2>/dev/null && [ -d ~/.rbenv/bin ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+fi
 
 # init according to man page
 if hash rbenv 2>/dev/null; then
