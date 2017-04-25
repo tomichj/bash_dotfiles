1939f083873c1c0fb13af811d6b9eca78eaccc5e
Justin Tomich <tomichj@lard.local>
2017-04-24 18:12:29 -0700

    puma-dev commands to kill or restart puma-dev

--- ruby/puma-dev.bash
+++ ruby/puma-dev.bash
@@ -7,14 +7,3 @@ puma-dev-restart() {
 puma-dev-kill() {
   pkill -USR1 puma-dev
 }
-
-# restart-puma-dev() {
-#   read -p "Are you sure? " -n 1 -r
-#   echo
-#   if [[ ! $REPLY =~ ^[Yy]$ ]]
-#   then
-#     exit 1
-#   fi
-#
-#   pids puma-dev | xargs kill
-# }
