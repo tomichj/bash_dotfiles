fb1eedb886c79778e408309ad21ecc03d131fff0
Justin Tomich <tomichj@gmail.com>
2017-10-13 17:22:36 -0700

    set up Brewfile, minimally wired up to bootstrap and install

---
+++ brew/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs Homebrew on a Mac, Linuxbrew on linux.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+else
+  echo "  Homebrew already installed."
+fi
+
+exit 0
